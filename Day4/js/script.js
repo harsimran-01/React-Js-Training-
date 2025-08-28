2fad588edbf6b5d3c3d67c4f8be5bdb6bb16b3a1
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2025-08-28 12:55:40 +0530

    day 4

--- Day4/js/script.js
+++ Day4/js/script.js
@@ -11,3 +11,74 @@ console.log(typeof(email));
 var age = 20;
 console.log(age);
 console.log(typeof(age));
+
+var choice = true
+console.log(choice);
+console.log(typeof(choice));
+
+var mobile;
+console.log(mobile);
+console.log(typeof(mobile));
+
+var n1 = 20;
+var n2 = 10;
+
+var add = n1+n2;
+console.log("add "+add);
+
+var sub = n1 - n2;
+console.log("subract " +sub);
+
+var product = n1*n2;
+console.log("product "+product);
+
+var division = n1/n2;
+console.log("dividend "+division);
+
+var remainder = n1 % n2;
+console.log("remainder "+remainder);
+
+
+var num = 20;
+var sq = num * num;
+var cube = num * num *num;
+console.log("Square of "+num + " "+ sq);
+console.log("Cube of "+num + " "+ cube);
+
+var num1 = 1;
+var num2 = 2;
+var num3 = 3;
+var num4 = 4;
+var num5 = 5;
+
+var sum = num1+num2+num3+num4+num5;
+console.log("Sum "+sum);
+var avg = sum / 5;
+console.log("Average = "+avg);
+
+var price = 230;
+var quantity = 2;
+var amount = price * quantity;
+console.log("amount = "+ amount);
+var discount = (10 / 100) * amount;
+console.log("amount of 10 % discount "+ discount);
+var final = amount - discount;
+console.log("Final amount after 10 % discount "+ final);
+
+console.log(10>6);
+console.log(10>16);
+
+console.log("***True case***");
+
+if(10 > 5){
+    console.log("10 is greater than 5");
+}
+
+console.log("Good day!");
+console.log("***False case***");
+
+if(1 > 5){
+    console.log("10 is greater than 5");
+}
+
+console.log("Good day!");
