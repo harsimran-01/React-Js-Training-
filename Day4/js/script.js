90f3880706364be1f47f34d1268a360254163e9d
Harsimran Kaur <140046087+harsimran-01@users.noreply.github.com>
2025-08-28 12:14:53 +0530

    day 3

---
+++ Day4/js/script.js
@@ -0,0 +1,13 @@
+console.log("Hello Sagar");
+
+var fullname = "admin";
+console.log(fullname);  
+console.log(typeof(fullname));
+
+var email = "admin@gmail.com";
+console.log(email);
+console.log(typeof(email));
+
+var age = 20;
+console.log(age);
+console.log(typeof(age));
